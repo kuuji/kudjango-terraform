1be37321accfa285d80ffbb4c8c8d859469502a2
Alexandre GARCIA <agarcia@norsys.fr>
2015-12-20 22:09:58 +0100

    output correction + remove sg elb

--- kudjango.tf
+++ kudjango.tf
@@ -28,28 +28,7 @@ resource "aws_subnet" "default" {
   map_public_ip_on_launch = true
 }
 
-# A security group for the ELB so it is accessible via the web
-resource "aws_security_group" "elb" {
-  name        = "terraform_example_elb"
-  description = "Used in the terraform"
-  vpc_id      = "${aws_vpc.default.id}"
 
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 # Security group
 resource "aws_security_group" "default" {
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "ip.front" {
-  value = "${aws_instance.kudjango-front.aws_instance}"
+output "ipfront" {
+  value = "${aws_instance.kudjango-front.public_ip}"
 }
 
-output "ip.app00" {
-  value = "${aws_instance.kudjango-app00.aws_instance}"
+output "ipapp00" {
+  value = "${aws_instance.kudjango-app00.public_ip}"
 }
 
-output "ip.app01" {
-  value = "${aws_instance.kudjango-app01.aws_instance}"
+output "ipapp01" {
+  value = "${aws_instance.kudjango-app01.public_ip}"
 }
 
-output "ip.db" {
-  value = "${aws_instance.kudjango-db.aws_instance}"
+output "ipdb" {
+  value = "${aws_instance.kudjango-db.public_ip}"
 }
